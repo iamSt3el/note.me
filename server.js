2e28ac2ba1240563bdf4b749954aa6d8240b2f3b
iamSt3el <himanshudhaka9166@gmail.com>
2024-07-22 00:53:16 +0530

    first commit

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use("/note", noteRoutes);
 
 // Serve static files from the React app in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'server/build')));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'server/build', 'index.html'));
   });
 }
 
